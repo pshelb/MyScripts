6048f4129f54428f00d3ba48ac689279e908bbda
pshelb <pshelberg@gmail.com>
2017-10-18 12:07:47 +1000

    Add prompts before installing each application

--- ubuntu_post_install.sh
+++ ubuntu_post_install.sh
@@ -1,66 +1,119 @@
 #!/bin/bash
 
-# TODO
-# Menu
-# Install IDE
-
 echo "Hello, $USER. Installing applications..."
 
-# AUTOMATIC INSTALLS
-
-# VMware Tools
-# echo "VMware Tools should be automatically installed."
+# Run as sudo from start
+sudo -v
 
 # GIMP
-echo "Installing GIMP."
-sudo apt-get install gimp
+read -p "Install GIMP? (y/n) " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]];
+then
+    echo "Installing GIMP."
+    sudo apt-get install gimp
+fi
+
+# GParted
+read -p "Install GParted? (y/n) " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]];
+then
+    echo "Installing GParted."
+    sudo apt-get install gparted
+fi
+
+# VLC
+read -p "Install VLC? (y/n) " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]];
+then
+    echo "Installing VLC."
+    sudo apt-get install vlc
+fi
+
+# Ubuntu Restricted Extras
+read -p "Install Ubuntu Restricted Extras? (y/n) " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]];
+then
+    echo "Installing Ubuntu Restricted Extras."
+    sudo apt-get install ubuntu-restricted-extras
+fi
 
 # Git
-echo "Installing Git."
-sudo add-apt-repository ppa:git-core/ppa
-sudo apt update
-sudo apt install git
+read -p "Install Git? (y/n) " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]];
+then
+    echo "Installing Git."
+    sudo add-apt-repository ppa:git-core/ppa
+    sudo apt update
+    sudo apt install git
+fi
 
 # Oracle JDK 8
-echo "Installing Oracle JDK."
-echo "Part A - Add the PPA."
-sudo add-apt-repository ppa:webupd8team/java
-echo "Part B - Update system package index."
-sudo apt update
-echo "Part C - Install Java installer script."
-sudo apt install oracle-java8-installer
-echo "Part D - Set environment variable (Requires a restart)."
-sudo apt install oracle-java8-set-default
-echo "Part E - Check version of Java."
-java -version
+read -p "Install Oracle JDK 8? (y/n) " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]];
+then
+    echo "Installing Oracle JDK 8."
+    echo "Part A - Add the PPA."
+    sudo add-apt-repository ppa:webupd8team/java
+    echo "Part B - Update system package index."
+    sudo apt update
+    echo "Part C - Install Java installer script."
+    sudo apt install oracle-java8-installer
+    echo "Part D - Set environment variable (Requires a restart)."
+    sudo apt install oracle-java8-set-default
+    echo "Part E - Check version of Java."
+    java -version
+fi
 
 # Sublime Text 3
-echo "Installing Sublime Text 3."
-read -p "Press enter to continue."
-echo "Part A - Install the GPG key."
-sudo wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-echo "Part B - Ensure apt is set up to work with https sources."
-sudo apt-get install apt-transport-https
-echo "Part C - Elect the channel to use."
-echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-echo "Part D - Update apt sources and install Sublime Text."
-sudo apt-get update
-sudo apt-get install sublime-text
+read -p "Install Sublime Text 3? (y/n) " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]];
+then
+    echo "Installing Sublime Text 3."
+    echo "Part A - Install the GPG key."
+    sudo wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+    echo "Part B - Ensure apt is set up to work with https sources."
+    sudo apt-get install apt-transport-https
+    echo "Part C - Elect the channel to use."
+    echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+    echo "Part D - Update apt sources and install Sublime Text."
+    sudo apt-get update
+    sudo apt-get install sublime-text
+fi
 
-# VS Code
-echo "Installing Visual Studio Code."
-curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
-sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-sudo apt-get update
-sudo apt-get install code
+# Visual Studio Code
+read -p "Install Visual Studio Code? (y/n) " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]];
+then
+    echo "Installing Visual Studio Code."
+    curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+    sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+    sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+    sudo apt-get update
+    sudo apt-get install code
+fi
  
-# MANUAL INSTALLS
-
 # Google Chrome
-echo "Install Google Chrome"
-echo "Download .deb from here: https://www.google.com/chrome/browser/desktop/index.html"
-echo "sudo dpkg -i google-chrome-stable_current_amd64.deb"
-echo "If there are dependency issues, run: sudo apt-get install -f"
-echo "and then run the original command again."
-read -p "Press enter to continue."
+read -p "Install Google Chrome? (y/n) " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]];
+then
+    echo "Installing Google Chrome."
+    wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+    if sudo dpkg -i google-chrome*.deb; then
+        echo 'Google Chrome installed.'
+    else
+        sudo apt-get install -f
+        sudo dpkg -i google-chrome*.deb
+        echo 'Google Chrome installed.'
+    fi
+fi
+
+read -p "Finished. Press enter to continue."
