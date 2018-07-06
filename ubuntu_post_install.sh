a12870b6712bf28619f19fe0462eb011c8d415ac
pshelb <pshelberg@gmail.com>
2018-07-06 15:27:53 +1000

    Update vs code install commands

--- ubuntu_post_install.sh
+++ ubuntu_post_install.sh
@@ -225,9 +225,9 @@ then
     echo "Installing Visual Studio Code."
     curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
     sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
-    sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+    sudo sh -c 'echo "deb [arch=amd64] http://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
     sudo apt-get update
-    sudo apt-get -y install code
+    sudo apt-get install code
 fi
 
 echo "Finished installing applications."
